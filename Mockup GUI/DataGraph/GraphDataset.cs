99cd4c866f5729276fb04c3b54b16aa7f38130fc
Michael Devenish <michael@michaeldevenish.com>
2017-06-25 18:45:04 +1000

    started working on GUI cleanup

---
+++ Mockup GUI/DataGraph/GraphDataset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGraph
+{
+    public class GraphDataset
+    {
+        private List<GraphNode> nodes;
+        public List<GraphNode> Nodes { get { return nodes; } }
+        public GraphDataset()
+        {
+            nodes = new List<GraphNode>();
+        }
+        public void AddNode(GraphNode node)
+        {
+            nodes.Add(node);
+        }
+    }
+}
