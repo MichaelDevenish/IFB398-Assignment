d15564d068da6ef2c7aeac53d7b916607470ef64
Michael Devenish <michael@michaeldevenish.com>
2017-06-26 01:53:30 +1000

    added all the static graph parts

--- Mockup GUI/DataGraph/DataGraph.cs
+++ Mockup GUI/DataGraph/DataGraph.cs
@@ -15,63 +15,96 @@ using System.Windows.Shapes;
 
 namespace DataGraph
 {
-    /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:DataGraph"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:DataGraph;assembly=DataGraph"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:CustomControl1/>
-    ///
-    /// </summary>
+
     public class TestGraph : Canvas
     {
+        const double XMIN = 60;
+        const double YMIN = 110;//dependant on how many lower items there are and if it is enabled
+
         private List<GraphDataset> datasets;
-        private int xDivisor = 10;
+        private int xDivisor = 1;
         private int yDivisor = 10;
+        private string xAxisName = "";
+        private string yAxisName = "";
 
         public int XDivisor { get { return xDivisor; } set { xDivisor = value; } }
         public int YDivisor { get { return yDivisor; } set { yDivisor = value; } }
+        public string XAxisName { set { xAxisName = value; } }
+        public string YAxisName { set { yAxisName = value; } }
         static TestGraph()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TestGraph), new FrameworkPropertyMetadata(typeof(TestGraph)));
-        }   
+        }
 
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
             datasets = new List<GraphDataset>();
-            
-        }
 
-        public void DrawGraph()
-        {
-            // the max x and y are set by the largest var in datasets
         }
 
         public void AddDataset(GraphDataset data)
         {
             datasets.Add(data);
         }
+       
+        public void DrawGraph()
+        {
+            double xdatamax = 20;//largest x value in datasets 
+            double ydatamax = 80;//largest y value in datasets
+            DrawXAxis(xdatamax);
+            DrawYAxis(ydatamax);
+            if (xAxisName != "") DrawGraphText(xAxisName, Width / 2, (Height - YMIN + 10));
+            if (yAxisName != "") DrawGraphText(yAxisName, 0, ((Height - YMIN) / 2) - 10);
+            // the max x and y are set by the largest var in datasets
+        }
+
+        private void DrawXAxis(double xdatamax)
+        {
+            GeometryGroup xaxis_geom = new GeometryGroup();
+            xaxis_geom.Children.Add(new LineGeometry(new Point(XMIN, Height - YMIN), new Point(Width, Height - YMIN)));
+            for (double x = 0; x <= xdatamax; x += xDivisor)
+            {
+                double point = (((Width - XMIN) / xdatamax) * (x)) + XMIN;
+                xaxis_geom.Children.Add(new LineGeometry(new Point(point, Height - YMIN - 5), new Point(point, Height - YMIN)));
+                DrawGraphText(x.ToString(), point - 2, (Height - YMIN));
+            }
+            DrawLine(xaxis_geom, 1, Brushes.Black);
+        }
+
+        private void DrawYAxis(double ydatamax)
+        {
+            GeometryGroup yaxis_geom = new GeometryGroup();
+            for (double y = 0; y <= ydatamax; y += yDivisor)
+            {
+                if (y != 0)
+                {
+                    double point = (Height - YMIN) - (((Height - YMIN) / ydatamax) * (y));
+                    yaxis_geom.Children.Add(new LineGeometry(new Point(Width, point), new Point(XMIN, point)));
+                    DrawGraphText(y.ToString(), XMIN - 20, (point - 8));
+                }
+            }
+            DrawLine(yaxis_geom, 0.5, Brushes.Gray);
+        }
+
+        private void DrawGraphText(string x, double LeftPoint, double TopPoint)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = x;
+            textBlock.Foreground = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+            SetLeft(textBlock, LeftPoint);
+            SetTop(textBlock, TopPoint);
+            Children.Add(textBlock);
+        }
+
+        private void DrawLine(GeometryGroup axis_geom, double thickness, SolidColorBrush brush)
+        {
+            Path axis_path = new Path();
+            axis_path.StrokeThickness = thickness;
+            axis_path.Stroke = brush;
+            axis_path.Data = axis_geom;
+            Children.Add(axis_path);
+        }
+
     }
 }
