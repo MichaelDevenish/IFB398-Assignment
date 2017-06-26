07597caabde089557cdcdab727ecc2d5b352cb61
Michael Devenish <michael@michaeldevenish.com>
2017-06-26 23:13:56 +1000

    preparation for graph drawing

--- Mockup GUI/DataGraph/DataGraph.cs
+++ Mockup GUI/DataGraph/DataGraph.cs
@@ -19,55 +19,87 @@ namespace DataGraph
     public class TestGraph : Canvas
     {
         const double XMIN = 60;
-        const double YMIN = 110;//dependant on how many lower items there are and if it is enabled
 
+        private double ymin = 110;//dependant on how many lower items there are (no larger if there are > 4) and if the activity summariser is enabled
         private List<GraphDataset> datasets;
         private int xDivisor = 1;
-        private int yDivisor = 10;
+        private int yDivisor = 1;
         private string xAxisName = "";
         private string yAxisName = "";
+        private bool summariser = true;
 
         public int XDivisor { get { return xDivisor; } set { xDivisor = value; } }
         public int YDivisor { get { return yDivisor; } set { yDivisor = value; } }
         public string XAxisName { set { xAxisName = value; } }
         public string YAxisName { set { yAxisName = value; } }
+
         static TestGraph()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(TestGraph), new FrameworkPropertyMetadata(typeof(TestGraph)));
         }
 
+        public void DisableSummariser()
+        {
+            summariser = false;
+        }
+
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            datasets = new List<GraphDataset>();
-
         }
 
         public void AddDataset(GraphDataset data)
         {
+            if (datasets == null) datasets = new List<GraphDataset>();
             datasets.Add(data);
         }
-       
+
         public void DrawGraph()
         {
-            double xdatamax = 20;//largest x value in datasets 
-            double ydatamax = 80;//largest y value in datasets
+            double xdatamax = 0;
+            double ydatamax = 0;
+            List<string> nodeNames = new List<string>();
+
+            if (datasets != null)
+            {
+                foreach (GraphDataset data in datasets)
+                {
+                    foreach (GraphNode node in data.Nodes)
+                    {
+                        double[] XY = node.GetCoords();
+                        if (XY[0] > xdatamax) xdatamax = XY[0];
+                        if (XY[1] > ydatamax) ydatamax = XY[1];
+                        if (!nodeNames.Contains(node.NodeName)) nodeNames.Add(node.NodeName);
+                    }
+                }
+                xdatamax = Math.Ceiling(xdatamax / xDivisor) * xDivisor;
+                ydatamax = Math.Ceiling(ydatamax / yDivisor) * yDivisor;
+
+                //TO BE USED WHEN THE BOTTOM HALF IS MADE DYNAMIC
+                //if( summariser == false){
+                //    ymin = 30;
+                //}else if (nodeNames.Count() < 4 )
+                //{
+                //    ymin -= (20 * (4 - nodeNames.Count()));
+                //}
+                //TODO draw each dataset onto the graph
+            }
+
             DrawXAxis(xdatamax);
             DrawYAxis(ydatamax);
-            if (xAxisName != "") DrawGraphText(xAxisName, Width / 2, (Height - YMIN + 10));
-            if (yAxisName != "") DrawGraphText(yAxisName, 0, ((Height - YMIN) / 2) - 10);
-            // the max x and y are set by the largest var in datasets
+            if (xAxisName != "") DrawGraphText(xAxisName, Width / 2, (Height - ymin + 10));
+            if (yAxisName != "") DrawGraphText(yAxisName, 0, ((Height - ymin) / 2) - 10);
         }
 
         private void DrawXAxis(double xdatamax)
         {
             GeometryGroup xaxis_geom = new GeometryGroup();
-            xaxis_geom.Children.Add(new LineGeometry(new Point(XMIN, Height - YMIN), new Point(Width, Height - YMIN)));
+            xaxis_geom.Children.Add(new LineGeometry(new Point(XMIN, Height - ymin), new Point(Width, Height - ymin)));
             for (double x = 0; x <= xdatamax; x += xDivisor)
             {
                 double point = (((Width - XMIN) / xdatamax) * (x)) + XMIN;
-                xaxis_geom.Children.Add(new LineGeometry(new Point(point, Height - YMIN - 5), new Point(point, Height - YMIN)));
-                DrawGraphText(x.ToString(), point - 2, (Height - YMIN));
+                xaxis_geom.Children.Add(new LineGeometry(new Point(point, Height - ymin - 5), new Point(point, Height - ymin)));
+                DrawGraphText(x.ToString(), point - 2, (Height - ymin));
             }
             DrawLine(xaxis_geom, 1, Brushes.Black);
         }
@@ -79,7 +111,7 @@ namespace DataGraph
             {
                 if (y != 0)
                 {
-                    double point = (Height - YMIN) - (((Height - YMIN) / ydatamax) * (y));
+                    double point = (Height - ymin) - (((Height - ymin) / ydatamax) * (y));
                     yaxis_geom.Children.Add(new LineGeometry(new Point(Width, point), new Point(XMIN, point)));
                     DrawGraphText(y.ToString(), XMIN - 20, (point - 8));
                 }
--- Mockup GUI/DataGraph/GraphDataset.cs
+++ Mockup GUI/DataGraph/GraphDataset.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace DataGraph
 {
     public class GraphDataset
     {
         private List<GraphNode> nodes;
+        private string datasetName;
+        private Brush colour;
+
         public List<GraphNode> Nodes { get { return nodes; } }
-        public GraphDataset()
+        public string DatasetName { get { return datasetName; } }
+        public Brush Colour { get { return colour; } }
+
+        public GraphDataset(string name, Brush colour)
         {
+            this.colour = colour;
+            datasetName = name;
             nodes = new List<GraphNode>();
         }
         public void AddNode(GraphNode node)
         {
+            node.SetButtonColour(colour);
             nodes.Add(node);
         }
     }
